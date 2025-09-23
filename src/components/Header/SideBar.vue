ab4b24f06e9aaa8edcc7ed774881fca2f87212cc
hamze-kabi <104631365+HamzeKabi@users.noreply.github.com>
2025-09-23 11:37:31 +0330

    finalize sidebar category style

--- src/components/Header/SideBar.vue
+++ src/components/Header/SideBar.vue
@@ -1,11 +1,11 @@
 <template>
   <menu
     ref="menu"
-    class="absolute top-1/13 lg:top-12.5 left-0 flex flex-col pt-3 bg-white overflow-hidden transition-all duration-500 xl:hidden"
+    class="absolute top-1/13 lg:top-12.5 left-0 flex flex-col pt-3 bg-white overflow-hidden transition-all duration-500 lg:pt-10 xl:hidden"
     :class="[sidebar.isOpen ? 'w-full' : 'w-0 p-0']"
   >
     <!-- categories container -->
-    <section class="flex flex-col items-center gap-26 pt-20">
+    <section class="flex flex-col items-center gap-26 pt-20 md:flex-row md:gap-10 md:px-5">
       <SideBarCategory :image="headphonesThumbnail" :product="'HEADPHONES'" />
       <SideBarCategory :image="speakersThumbnail" :product="'SPEAKERS'" />
       <SideBarCategory :image="earphonesThumbnail" :product="'EARPHONES'" />
--- src/components/Header/SideBarCategory.vue
+++ src/components/Header/SideBarCategory.vue
@@ -1,9 +1,15 @@
 <template>
-  <div class="flex flex-col bg-gray-100 items-center h-40 w-80 rounded-3xl">
-    <img class="-translate-y-33 scale-55" :src="image" alt="category image" />
-    <p class="-translate-y-56 font-bold">{{ product }}</p>
-    <RouterLink class="-translate-y-56 font-bold text-gray-500 text-sm" to=""
-      >SHOP <span class="-translate-y-56 font-bold text-lg text-orange-600">></span></RouterLink
+  <div class="flex flex-col bg-gray-100 items-center justify-center h-40 w-80 rounded-3xl">
+    <img
+      class="-translate-y-8 scale-55 md:scale-70 md:-translate-y-10"
+      :src="image"
+      alt="category image"
+    />
+    <p class="-translate-y-32 font-bold md:-translate-y-23 lg:-translate-y-28">{{ product }}</p>
+    <RouterLink
+      class="-translate-y-32 font-bold text-gray-500 text-sm md:-translate-y-23 lg:-translate-y-28"
+      to=""
+      >SHOP <span class="-translate-y-32 font-bold text-lg text-orange-600">></span></RouterLink
     >
   </div>
 </template>
