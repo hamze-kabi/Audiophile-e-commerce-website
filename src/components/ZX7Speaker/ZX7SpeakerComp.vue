d76db1a82ffe6df3931d3d926624ca6f963f440a
hamze-kabi <104631365+HamzeKabi@users.noreply.github.com>
2025-09-24 02:12:44 +0330

    start zx7speaker comp

--- src/components/ZX7Speaker/ZX7SpeakerComp.vue
+++ src/components/ZX7Speaker/ZX7SpeakerComp.vue
@@ -1,3 +1,12 @@
-<template></template>
+<template>
+  <div
+    class="bg-[url(@/assets/home/mobile/image-speaker-zx7.jpg)] bg-no-repeat h-90 bg-cover bg-right"
+  >
+    <div class="flex flex-col">
+      <h2>ZX7 SPEAKER</h2>
+      <button class="bg-black px-7 py-3 font-bold text-sm mb-15">SEE PRODUCT</button>
+    </div>
+  </div>
+</template>
 
 <script setup></script>
