4b82dfd7065967220577c5f92b831671f5542b6a
hamze-kabi <104631365+HamzeKabi@users.noreply.github.com>
2025-09-23 22:49:38 +0330

    add zx9speaker tablet version

--- src/components/ZX9Speaker/ZX9SpeakerComp.vue
+++ src/components/ZX9Speaker/ZX9SpeakerComp.vue
@@ -1,14 +1,16 @@
 <template>
   <div
-    class="flex flex-col items-center justify-center bg-[#D87D4A] gap-5 text-white rounded-2xl max-w-87 mx-auto"
+    class="flex flex-col items-center justify-center bg-[#D87D4A] gap-5 text-white rounded-2xl max-w-87 mx-auto md:max-w-180 md:bg-[url(@/assets/home/desktop/pattern-circles.svg)] md:bg-center md:bg-size-[43rem] md:bg-no-repeat"
   >
     <img
-      class="scale-50 -mt-10"
+      class="scale-50 -mt-10 md:scale-60 md:mt-2"
       src="@/assets/home/mobile/image-speaker-zx9.png"
       alt="ZX9Speaker"
     />
-    <h2 class="text-[2.5rem] tracking-wider font-semibold -mt-25">ZX9 SPEAKER</h2>
-    <p class="text-center">
+    <h2 class="text-[2.5rem] tracking-wider font-semibold -mt-25 md:hidden">ZX9 SPEAKER</h2>
+    <h2 class="hidden text-7xl tracking-wider font-semibold -mt-17 md:block">ZX9</h2>
+    <h2 class="hidden text-7xl tracking-wider font-semibold -mt-4 md:block">SPEAKER</h2>
+    <p class="text-center md:max-w-90 md:font-semibold">
       Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.
     </p>
     <button class="bg-black px-7 py-3 font-bold text-sm mb-15">SEE PRODUCT</button>
