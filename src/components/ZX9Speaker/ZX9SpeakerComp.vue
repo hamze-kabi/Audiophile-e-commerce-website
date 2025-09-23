54c1ba40109d6fc3c7ac3f3cb4b3b0d4decd3ab2
hamze-kabi <104631365+HamzeKabi@users.noreply.github.com>
2025-09-24 01:08:22 +0330

    correct flex of texts of zx9speaker comp

--- src/components/ZX9Speaker/ZX9SpeakerComp.vue
+++ src/components/ZX9Speaker/ZX9SpeakerComp.vue
@@ -1,20 +1,30 @@
 <template>
   <div
-    class="flex flex-col items-center justify-center bg-[#D87D4A] gap-5 text-white rounded-2xl max-w-87 mx-auto md:max-w-180 md:bg-[url(@/assets/home/desktop/pattern-circles.svg)] md:bg-center md:bg-size-[43rem] md:bg-no-repeat"
+    class="flex flex-col items-center justify-center bg-[#D87D4A] gap-5 text-white rounded-2xl max-w-87 mx-auto mt-30 md:max-w-180 md:bg-[url(@/assets/home/desktop/pattern-circles.svg)] md:bg-center md:bg-size-[43rem] md:bg-no-repeat xl:flex-row"
   >
     <img
       class="scale-50 -mt-10 md:scale-60 md:mt-2"
       src="@/assets/home/mobile/image-speaker-zx9.png"
       alt="ZX9Speaker"
     />
-    <h2 class="text-[2.5rem] tracking-wider font-semibold -mt-25 md:hidden">ZX9 SPEAKER</h2>
-    <h2 class="hidden text-7xl tracking-wider font-semibold -mt-17 md:block">ZX9</h2>
-    <h2 class="hidden text-7xl tracking-wider font-semibold -mt-4 md:block">SPEAKER</h2>
-    <p class="text-center md:max-w-90 md:font-semibold">
-      Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.
-    </p>
-    <button class="bg-black px-7 py-3 font-bold text-sm mb-15">SEE PRODUCT</button>
+    <div class="flex flex-col items-center justify-center gap-5 -mt-20">
+      <h2 class="text-[2.5rem] tracking-wider font-semibold md:hidden">ZX9 SPEAKER</h2>
+      <h2 class="hidden text-6xl tracking-wider font-semibold md:block">ZX9</h2>
+      <h2 class="hidden text-6xl tracking-wider font-semibold -mt-4 md:block">SPEAKER</h2>
+      <p class="text-center md:max-w-90 md:font-semibold">
+        Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.
+      </p>
+      <button class="bg-black px-7 py-3 font-bold text-sm mb-15">SEE PRODUCT</button>
+    </div>
   </div>
 </template>
 
 <script setup></script>
+
+<!-- <h2 class="text-[2.5rem] tracking-wider font-semibold -mt-25 md:hidden">ZX9 SPEAKER</h2>
+      <h2 class="hidden text-7xl tracking-wider font-semibold -mt-17 md:block">ZX9</h2>
+      <h2 class="hidden text-7xl tracking-wider font-semibold -mt-4 md:block">SPEAKER</h2>
+      <p class="text-center md:max-w-90 md:font-semibold">
+        Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.
+      </p>
+      <button class="bg-black px-7 py-3 font-bold text-sm mb-15">SEE PRODUCT</button> -->
