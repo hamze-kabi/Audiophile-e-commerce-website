bbc941675ba4d5f61478d30f5d31cb4d01a54cef
hamze-kabi <104631365+HamzeKabi@users.noreply.github.com>
2025-09-24 14:27:09 +0330

    add zx7speaker comp desktop style

--- src/components/ZX7Speaker/ZX7SpeakerComp.vue
+++ src/components/ZX7Speaker/ZX7SpeakerComp.vue
@@ -1,8 +1,8 @@
 <template>
   <div
-    class="max-w-87 mx-auto rounded-2xl bg-[url(@/assets/home/mobile/image-speaker-zx7.jpg)] bg-no-repeat h-90 bg-cover bg-right md:bg-[url(@/assets/home/tablet/image-speaker-zx7.jpg)] md:max-w-180"
+    class="max-w-87 mx-auto rounded-2xl bg-[url(@/assets/home/mobile/image-speaker-zx7.jpg)] bg-no-repeat h-90 bg-cover bg-right md:bg-[url(@/assets/home/tablet/image-speaker-zx7.jpg)] md:max-w-180 xl:bg-[url(@/assets/home/desktop/image-speaker-zx7.jpg)] xl:max-w-290"
   >
-    <div class="flex flex-col items-start ml-10 mt-10 pt-33 justify-center">
+    <div class="flex flex-col items-start ml-10 mt-10 pt-33 justify-center xl:ml-30">
       <h2 class="text-[2rem] tracking-wider font-semibold">ZX7 SPEAKER</h2>
       <button class="border border-2 px-7 py-3 font-bold text-sm mt-6">SEE PRODUCT</button>
     </div>
