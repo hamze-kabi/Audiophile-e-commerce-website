f95b08ced52d3d7647373c7646e55df6c67af024
hamze-kabi <104631365+HamzeKabi@users.noreply.github.com>
2025-09-24 14:06:30 +0330

    add zx7speaker comp tablet style

--- src/components/ZX7Speaker/ZX7SpeakerComp.vue
+++ src/components/ZX7Speaker/ZX7SpeakerComp.vue
@@ -1,10 +1,10 @@
 <template>
   <div
-    class="bg-[url(@/assets/home/mobile/image-speaker-zx7.jpg)] bg-no-repeat h-90 bg-cover bg-right"
+    class="max-w-87 mx-auto rounded-2xl bg-[url(@/assets/home/mobile/image-speaker-zx7.jpg)] bg-no-repeat h-90 bg-cover bg-right md:bg-[url(@/assets/home/tablet/image-speaker-zx7.jpg)] md:max-w-180"
   >
-    <div class="flex flex-col">
-      <h2>ZX7 SPEAKER</h2>
-      <button class="bg-black px-7 py-3 font-bold text-sm mb-15">SEE PRODUCT</button>
+    <div class="flex flex-col items-start ml-10 mt-10 pt-33 justify-center">
+      <h2 class="text-[2rem] tracking-wider font-semibold">ZX7 SPEAKER</h2>
+      <button class="border border-2 px-7 py-3 font-bold text-sm mt-6">SEE PRODUCT</button>
     </div>
   </div>
 </template>
