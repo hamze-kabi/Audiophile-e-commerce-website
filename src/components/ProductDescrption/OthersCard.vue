5ae015946902841662ef6cad3c5b247eff569237
hamze-kabi <104631365+HamzeKabi@users.noreply.github.com>
2025-11-14 19:20:44 +0330

    Update OthersCard.vue

--- src/components/ProductDescrption/OthersCard.vue
+++ src/components/ProductDescrption/OthersCard.vue
@@ -1,15 +1,21 @@
 <template>
-  <picture>
-    <source media="(min-width:1440px)" :srcset="imageSet.desktop" />
-    <source media="(min-width:768px)" :srcset="imageSet.tablet" />
-    <img class="w-80 rounded-2xl md:w-170 xl:w-135" :src="imageSet.mobile" alt="" />
-  </picture>
+  <!-- container of images title and see product button -->
+  <div>
+    <picture>
+      <source media="(min-width:1440px)" :srcset="imageSet.desktop" />
+      <source media="(min-width:768px)" :srcset="imageSet.tablet" />
+      <img class="w-80 rounded-2xl md:w-170 xl:w-135" :src="imageSet.mobile" :alt="imageAlt" />
+    </picture>
+    <p>{{ productName }}</p>
+    <LinkButton />
+  </div>
 </template>
 
 <script setup>
 import HeadphonesData from '@/assets/data/HeadphonesData.json'
 import SpeakersData from '@/assets/data/SpeakersData.json'
 import EarphonesData from '@/assets/data/EarphonesData.json'
+import LinkButton from '../LinkButtons/LinkButton.vue'
 import { ref } from 'vue'
 
 const props = defineProps({
@@ -18,6 +24,7 @@ const props = defineProps({
 
 const imageSet = ref()
 const imageAlt = ref('')
+const productName = ref('')
 
 const findProduct = function () {
   ;[HeadphonesData, SpeakersData, EarphonesData].forEach((data) => {
@@ -25,6 +32,7 @@ const findProduct = function () {
       if (product.id === props.receivedId) {
         imageSet.value = product.otherImages
         imageAlt.value = product.title
+        productName.value = product.slug
       }
     })
   })
