05be8d2c84d2071e572279680787f01d90963859
hamze-kabi <104631365+HamzeKabi@users.noreply.github.com>
2025-12-02 11:24:40 +0330

    add credit

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <RouterView />
+  <footer class="w-full py-2 flex items-center justify-center text-white bg-black gap-1">
+    Created by Hamze Kabi | <a href="https://github.com/hamze-kabi" target="_blank">GitHub</a> |
+    <a href="mailto:hamzekabinasab@gmail.com">Email</a>
+  </footer>
 </template>
 
 <script setup></script>
